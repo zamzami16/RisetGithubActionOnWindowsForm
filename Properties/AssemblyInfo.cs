05ad20a7a507be124d80b5bf012f2f68693bdcb1
zamzami16 <yusufazzamzami@gmail.com>
2025-04-15 13:46:37 +0700

    bump version to v.0.0.6.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.6.1")]
+[assembly: AssemblyVersion("0.0.6.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
