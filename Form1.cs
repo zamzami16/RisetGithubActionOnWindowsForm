bc14d489dbe4d156428ea0b421644c1455dde987
zamzami16 <yusufazzamzami@gmail.com>
2025-04-16 12:19:33 +0700

    add external deps and bump version to v0.0.8.0

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace RisetGithubActionOnWindowsForm
+﻿namespace RisetGithubActionOnWindowsForm
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Axata.UIControls.Forms.AxataForm1
     {
         public Form1()
         {
